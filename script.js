a9ae51112e0c07ac8b3dbe891926da298ebb6f4b
Bruno Minelli <minellibruno@hotmail.com>
2021-12-10 18:33:44 -0300

    Requisito 07 concluído.

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // Constantes e Variáveis
+// Seleção de elementos HTML
 const buttonCreateTask = document.getElementById('criar-tarefa');
 const elementTaskList = document.getElementById('lista-tarefas');
 const elementTaksText = document.getElementById('texto-tarefa');
 
-console.log(buttonCreateTask);
-console.log(elementTaksText);
-console.log(elementTaskList);
 // Funções
-function createTask() {
-  buttonCreateTask.addEventListener('click', () => {
-    if (elementTaksText.value) {
-      const elementLi = document.createElement('li');
-      elementLi.innerHTML = elementTaksText.value;
-      elementTaskList.appendChild(elementLi);
-      elementTaksText.value = '';
-    } else alert('Erro: Insira uma tarefa!');
-  });
+function removeBackgroundcolor() {
+    const elementLi = document.getElementsByTagName('li');
+    for (let index = 0; index < elementLi.length; index += 1) {
+        elementLi[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    }
 }
+
+buttonCreateTask.addEventListener('click', () => {
+  if (elementTaksText.value) {
+    const elementLi = document.createElement('li');
+    elementLi.innerHTML = elementTaksText.value;
+    elementTaskList.appendChild(elementLi);
+    elementTaksText.value = '';
+    elementLi.addEventListener('click', () => {
+        if (elementLi.style.backgroundColor === '') {
+            elementLi.style.backgroundColor = 'rgb(128, 128, 128)';
+        } else if (elementLi.style.backgroundColor === 'rgb(128, 128, 128)') {
+            elementLi.style.backgroundColor = '';
+        }
+    })
+  } else alert('Erro: Insira uma tarefa!');
+});
+
 // Execução
-createTask();
