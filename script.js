302a5b5f7804243c94fa9825ed97d9817c7ba9e7
Bruno Minelli <minellibruno@hotmail.com>
2021-12-10 16:26:57 -0300

    Requisitos 05 e 06 concluídos.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Constantes e Variáveis
+const buttonCreateTask = document.getElementById('criar-tarefa');
+const elementTaskList = document.getElementById('lista-tarefas');
+const elementTaksText = document.getElementById('texto-tarefa');
 
+console.log(buttonCreateTask);
+console.log(elementTaksText);
+console.log(elementTaskList);
 // Funções
 
+buttonCreateTask.addEventListener('click', () => {
+  const elementLi = document.createElement('li');
+  elementLi.innerHTML = elementTaksText.value;
+  elementTaskList.appendChild(elementLi);
+  elementTaksText.value = '';
+});
 // Execução
