b7dc62ed37b0699a68efa927afa2cb0e5a37682c
Bruno Minelli <minellibruno@hotmail.com>
2021-12-11 13:42:03 -0300

    Requisito 13 finalizado.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // Constantes e Variáveis
-// Seleção de elementos HTML
 
 // Botões
 const buttonCreateTask = document.getElementById('criar-tarefa');
 const buttonClearAllTasks = document.getElementById('apaga-tudo');
 const buttonClearCompletedTasks = document.getElementById('remover-finalizados');
 const buttonSaveTasks = document.getElementById('salvar-tarefas');
+const buttonMoveTaskUp = document.getElementById('mover-cima');
+const buttonMoveTaskDown = document.getElementById('mover-baixo');
 
 // Lista de Tarefas
 const taskList = document.getElementById('lista-tarefas');
 const inputTask = document.getElementById('texto-tarefa');
 
 // Funções
-/* Cria itens da lista de tarefas */
+/* Botão Criar: Cria itens da lista de tarefas */
 buttonCreateTask.addEventListener('click', () => {
   if (inputTask.value !== '') {
     const listItem = document.createElement('li');
@@ -23,14 +24,14 @@ buttonCreateTask.addEventListener('click', () => {
   } else alert('Insira uma tarefa!');
 });
 
-/* Apaga todos os itens da lista de tarefas */
+/* Botão Limpar: Apaga todos os itens da lista de tarefas */
 buttonClearAllTasks.addEventListener('click', () => {
   for (let index = taskList.childNodes.length - 1; index >= 0; index -= 1) {
     taskList.removeChild(taskList.childNodes[index]);
   }
 });
 
-/* Apaga todos os itens finalizados da lista de tarefas */
+/* Botão Limpar Finalizadas: Apaga todos os itens finalizados da lista de tarefas */
 buttonClearCompletedTasks.addEventListener('click', () => {
   const listItem = document.querySelectorAll('.completed');
   for (let index = listItem.length - 1; index >= 0; index -= 1) {
@@ -38,7 +39,7 @@ buttonClearCompletedTasks.addEventListener('click', () => {
   }
 });
 
-/* Salva a lista de tarefas */
+/* Botão Salvar: Salva a lista de tarefas */
 buttonSaveTasks.addEventListener('click', () => {
   window.localStorage.clear();
   const listItems = taskList.childNodes;
@@ -65,7 +66,46 @@ window.onload = () => {
   }
 };
 
-console.log();
+/* Função auxiliar para os eventos dos seguintes botões:
+-- Botão Subir Item
+-- Botão Descer Item
+
+Verifica a posição do item antes de movê-lo*/
+function verifyItemPosition(item) {
+  for (let index = 0; index < taskList.childNodes.length; index += 1) {
+    if (item.innerText === taskList.childNodes[index].innerText) {
+      console.log(`${index} position`);
+      return index;
+    }
+  }
+}
+
+/* Botão Subir Item: Reposiciona um item da lista para cima */
+buttonMoveTaskUp.addEventListener('click', () => {
+  const selctedItem = document.querySelector('.selected');
+  if (selctedItem !== null) {
+    const itemPosition = verifyItemPosition(selctedItem);
+    if (itemPosition !== 0) {
+      const currentItem = selctedItem.previousElementSibling;
+      taskList.removeChild(selctedItem);
+      currentItem.insertAdjacentElement('beforebegin', selctedItem);
+    } else alert('Não é possível mover o ítem');
+  }
+});
+
+/* Botão Descer Item: Reposiciona um item da lista para baixo */
+buttonMoveTaskDown.addEventListener('click', () => {
+  const selctedItem = document.querySelector('.selected');
+  if (selctedItem !== null) {
+    const itemPosition = verifyItemPosition(selctedItem);
+    const elementsLi = document.getElementsByTagName('li');
+    if (itemPosition !== elementsLi.length - 1) {
+      const currentItem = selctedItem.nextElementSibling;
+      taskList.removeChild(selctedItem);
+      currentItem.insertAdjacentElement('afterend', selctedItem);
+    } else alert('Não é possível mover o ítem');
+  }
+});
 
 /* Seleciona um elemento da lista de tarefas e adiciona a classe selected */
 taskList.addEventListener('click', (event) => {
